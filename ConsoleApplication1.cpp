cfb99dafe42e9de24bbb1035ebb911a323299da3
Sergpx <93960726+Sergpx@users.noreply.github.com>
2024-03-06 15:48:40 +0300

    DZ DATE

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int a = 1000000000;
+
+    int array[a];
+
+    /*for (int i = 0; i < a; i++) {
+        array[i] = i + 1;
+    }*/
+
+    /*for (int i = 0; i < a; i++) {
+        cout << array[i] << " ";
+    }*/
+    //cout << endl;
+}
